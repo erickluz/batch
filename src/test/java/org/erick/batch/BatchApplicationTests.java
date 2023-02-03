38759e7a95bd55f9978ea21b546cc64b8a321fed
Erick Candido da Luz <erickluz@pop-os.localdomain>
2023-02-02 22:24:28 -0300

    Odd and Even

---
+++ src/test/java/org/erick/batch/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.erick.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
